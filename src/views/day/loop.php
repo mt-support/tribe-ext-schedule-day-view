a79696c386fa615de33ebef6415b851b156aca9b
Ryan Urban <ryan@fringewebdesign.com>
2018-01-31 12:44:55 -0600

    fix merge conflicts

--- src/views/day/loop.php
+++ src/views/day/loop.php
@@ -15,26 +15,32 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 global $more, $post, $wp_query;
-$more = false;
-$current_timeslot = null;
 
-$today = Tribe__Extension__Schedule_Day_View::today();
+$more             = false;
+$current_timeslot = null;
+$today            = Tribe__Extension__Schedule_Day_View::today();
 
 ?>
 
-<div id="tribe-events-day" class="tribe-events-loop">
+<div
+    id="tribe-events-day"
+    class="tribe-events-loop"
+    data-site-timezone="<?php echo esc_attr( Tribe__Events__Timezones::wp_timezone_string() ); ?>"
+    data-now="<?php echo esc_attr( time() ); ?>"
+>
+
 	<div class="tribe-events-day-time-slot">
 
 	<?php do_action( 'tribe_ext_sch_day_inside_before_loop' ); ?>
 	<?php while ( have_posts() ) : the_post(); ?>
+
     <?php
-    if ( $current_timeslot != $post->timeslot ) :
+    if ( $current_timeslot !== $post->timeslot ) :
 		$current_timeslot = $post->timeslot;
         $is_all_day_timeslot = $current_timeslot === 'All Day';
         $class_group_active_on_load = ( $today ) || $is_all_day_timeslot ? ' tribe-events-day-grouping-is-active' : '';
 		?>
-	</div>
-	<!-- .tribe-events-day-time-slot -->
+	</div><!-- .tribe-events-day-time-slot -->
 
 	<div class="tribe-events-day-time-slot<?php echo $class_group_active_on_load; ?>">
         <h5>
@@ -51,6 +57,6 @@ $today = Tribe__Extension__Schedule_Day_View::today();
 		<?php do_action( 'tribe_ext_sch_day_inside_after_loop' ); ?>
 	<?php endwhile; ?>
 
-	</div>
-	<!-- .tribe-events-day-time-slot -->
+	</div><!-- .tribe-events-day-time-slot -->
+
 </div><!-- .tribe-events-loop -->
