dcc1a173e063a0d811071156ada3a71c905ace82
Ryan Urban <ryan@fringewebdesign.com>
2018-01-31 14:34:03 -0600

    styling

--- src/views/day/single.php
+++ src/views/day/single.php
@@ -17,8 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 $venue_details = tribe_get_venue_details();
 
 // Venue microformats
-$has_venue         = ( $venue_details ) ? ' vcard' : '';
-$has_venue_address = ( ! empty( $venue_details['address'] ) ) ? ' location' : '';
+$has_venue = $venue_details ? ' vcard' : '';
 
 // The address string via tribe_get_venue_details will often be populated even when there's
 // no address, so let's get the address string on its own for a couple of checks below.
@@ -26,55 +25,57 @@ $venue_address = tribe_get_address();
 
 ?>
 
-	<!-- Event Title -->
-<?php do_action( 'tribe_ext_sch_day_single_before_the_event_title' ) ?>
-	<h2 class="tribe-events-list-event-title summary ">
-		<a class="url" href="<?php echo esc_url( tribe_get_event_link() ); ?>" title="<?php the_title_attribute() ?>" rel="bookmark">
-			<?php the_title() ?>
-		</a>
-	</h2>
-<?php do_action( 'tribe_ext_sch_day_single_after_the_event_title' ) ?>
+<!-- Schedule & Recurrence Details -->
+<div class="tribe-updated published time-details">
+    <?php echo tribe_events_event_schedule_details(); ?>
+</div>
 
-	<!-- Event Meta -->
-<?php do_action( 'tribe_ext_sch_day_single_before_the_meta' ) ?>
-	<div class="tribe-events-event-meta <?php echo esc_attr( $has_venue . $has_venue_address ); ?>">
+<div class="tribe-events-day-event-content">
 
-		<!-- Schedule & Recurrence Details -->
-		<div class="tribe-updated published time-details">
-			<?php echo tribe_events_event_schedule_details(); ?>
-		</div>
+    <!-- Event Title -->
+    <?php do_action( 'tribe_ext_sch_day_single_before_the_event_title' ) ?>
+        <h2 class="tribe-events-list-event-title summary ">
+            <a class="url" href="<?php echo esc_url( tribe_get_event_link() ); ?>" title="<?php the_title_attribute() ?>" rel="bookmark">
+                <?php the_title() ?>
+            </a>
+        </h2>
+    <?php do_action( 'tribe_ext_sch_day_single_after_the_event_title' ) ?>
 
-		<?php if ( $venue_details ) : ?>
-			<!-- Venue Display Info -->
-			<div class="tribe-events-venue-details">
-				<?php
-				$address_delimiter = empty( $venue_address ) ? ' ' : ', ';
+    <!-- Event Meta -->
+    <?php do_action( 'tribe_ext_sch_day_single_before_the_meta' ) ?>
+    <div class="tribe-events-event-meta <?php echo esc_attr( $has_venue ); ?>">
 
-				// These details are already escaped in various ways earlier in the code.
-				echo implode( $address_delimiter, $venue_details );
-				?>
-			</div> <!-- .tribe-events-venue-details -->
-		<?php endif; ?>
+        <?php if ( $venue_details ) : ?>
+            <!-- Venue Display Info -->
+            <div class="tribe-events-venue-details">
+                <?php
+                $address_delimiter = empty( $venue_address ) ? ' ' : ', ';
 
-	</div><!-- .tribe-events-event-meta -->
+                // These details are already escaped in various ways earlier in the code.
+                echo implode( $address_delimiter, $venue_details );
+                ?>
+            </div> <!-- .tribe-events-venue-details -->
+        <?php endif; ?>
 
-<?php if ( tribe_get_cost() ) : ?>
-	<div class="tribe-events-event-cost">
-		<span class="ticket-cost"><?php echo tribe_get_cost( null, true ); ?></span>
-		<?php
-		/** This action is documented in the-events-calendar/src/views/list/single-event.php */
-		do_action( 'tribe_ext_sch_day_single_inside_cost' )
-		?>
-	</div>
-<?php endif; ?>
+    </div><!-- .tribe-events-event-meta -->
 
-<?php do_action( 'tribe_ext_sch_day_single_after_the_meta' ) ?>
+    <?php if ( tribe_get_cost() ) : ?>
+        <div class="tribe-events-event-cost">
+            <span class="ticket-cost"><?php echo tribe_get_cost( null, true ); ?></span>
+            <?php
+            /** This action is documented in the-events-calendar/src/views/list/single-event.php */
+            do_action( 'tribe_ext_sch_day_single_inside_cost' )
+            ?>
+        </div>
+    <?php endif; ?>
 
-	<!-- Event Content -->
-<?php do_action( 'tribe_ext_sch_day_single_before_the_content' ) ?>
-	<div class="tribe-events-list-event-description tribe-events-content description entry-summary">
-		<?php echo tribe_events_get_the_excerpt(); ?>
-		<a href="<?php echo esc_url( tribe_get_event_link() ); ?>" class="tribe-events-read-more" rel="bookmark"><?php esc_html_e( 'Find out more', 'the-events-calendar' ) ?> &raquo;</a>
-	</div><!-- .tribe-events-list-event-description -->
-<?php
-do_action( 'tribe_ext_sch_day_single_after_the_content' );
+    <?php do_action( 'tribe_ext_sch_day_single_after_the_meta' ) ?>
+
+    <!-- Event Content -->
+    <?php do_action( 'tribe_ext_sch_day_single_before_the_content' ) ?>
+        <a href="<?php echo esc_url( tribe_get_event_link() ); ?>" class="tribe-events-read-more" rel="bookmark">
+            <?php esc_html_e( 'Event Details', 'the-events-calendar' ) ?> &raquo;
+        </a>
+    <?php do_action( 'tribe_ext_sch_day_single_after_the_content' ); ?>
+
+</div>
